cdf555a957d6e69c523fadf426c710f6d183485c
echohuiecho <hiechohui@gmail.com>
2021-07-25 21:10:10 +0800

    upload build folder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Vue-hexschool-week7_VueCLI/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Amorphous_Laundry/' : '/',
 };
